b4947a4166fa7ed731fd2efb7f4a919a964f8690
devpeak <malaythakur13@gmail.com>
2023-02-24 18:58:27 +0530

    setTimeout, Regualar function and arrow function

---
+++ async_js.js
@@ -0,0 +1,26 @@
+//asynchronous Javascript
+
+//pass function as an argument (arrow function), counted time in miliseconds, 3000ms is equivalent to 3seconds
+
+    console.log(" I ");
+    console.log(" eat ");
+
+    setTimeout(()=>{console.log(" ice cream ");},4000)
+
+    console.log(" with a ");
+
+    console.log(" spoon "); 
+
+//REGULAR FUNCTION 
+
+/*
+function sum(num1, num2){
+
+}
+*/
+
+//ARROW FUNCTION
+
+/*
+let sum = (num1,num2) => {}
+*/
