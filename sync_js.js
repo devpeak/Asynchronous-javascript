3177e8bce1f6890f01a26cbbcd5d666318bf1848
devpeak <malaythakur13@gmail.com>
2023-02-24 18:56:32 +0530

    javascript executes serially

---
+++ sync_js.js
@@ -0,0 +1,13 @@
+//synchoronous
+
+console.log(" I ")
+
+console.log(" eat ")
+
+console.log(" ice cream ")
+
+console.log(" with a ")
+
+console.log(" spoon ")
+
+//javascript runs from top to the bottom serially.In this process anything get stuck then entire process get stuck.
