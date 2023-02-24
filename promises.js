6ad70174c7c1ce3f86c206523bdf695ce06ba325
devpeak <malaythakur13@gmail.com>
2023-02-24 21:25:17 +0530

    Promise chaininig

--- promises.js
+++ promises.js
@@ -30,3 +30,32 @@ let order = (time,work) => {
 };
 
 order(2000, ()=>console.log(`${stocks.Fruits[3]} was selected`))
+
+.then(()=>{
+    return order(0000, ()=>console.log("Production has started"));
+})
+
+.then(()=>{
+    return order(2000, ()=>console.log("Fruits has been chopped"));
+})
+
+.then(()=>{
+    return order(1000, ()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`));
+})
+
+.then(()=>{
+    return order(1000, ()=>console.log("start the machine"));
+})
+
+.then(()=>{
+    return order(2000, ()=>console.log(`ice cream placed on ${stocks.holder[0]}`));
+})
+
+.then(()=>{
+    return order(3000, ()=>console.log(`${stocks.topping[0]} was selected`));
+
+})
+
+.then(()=>{
+    return order(1000,()=>console.log("ice cream was served :)"))
+})
