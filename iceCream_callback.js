92f15b1a1988b8d762f625034bd7a33468ecd0ea
devpeak <malaythakur13@gmail.com>
2023-02-24 19:36:25 +0530

    callback hell- relationship bw parents and children and grandchildren and grand grand children so on

--- iceCream_callback.js
+++ iceCream_callback.js
@@ -12,7 +12,7 @@ let order = (Fruits_name, call_production)=>{
         console.log(`${stocks.Fruits[Fruits_name]} was selected`);
 
         call_production();
-        
+
     }, 2000);
 
     
@@ -21,7 +21,43 @@ let order = (Fruits_name, call_production)=>{
 let production = ()=>{
    setTimeout(() => {
         console.log("Production has started");
+
+        setTimeout(() => {
+            console.log("The fruits has been chopped");
+
+            setTimeout(()=>{
+
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} is added`);
+
+                setTimeout(()=>{
+
+                    console.log("The machine has been started");
+                },1000);
+
+                    setTimeout(()=>{
+                        
+                        console.log(`ice cream placed in ${stocks.holder[1]} `)
+
+                        setTimeout(()=>{
+
+                            console.log(`${stocks.topping[1]} is added on top`);
+
+                            setTimeout(() => {
+                                
+                                console.log("Ice cream is served :)");
+
+                            }, 2000);
+
+                        },3000)
+
+                    },2000);
+
+            },1000);
+
+       }, 2000);
+    ;
    }, 0000);
-};
+
+}
 
 order(0,production);
